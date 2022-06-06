fdc65944e67c739e4f01d6d1b1695f20d1075870
franciowel <spina_francesco@blu.it>
2022-06-06 15:39:04 +0200

    creato calcolatore

--- js/script.js
+++ js/script.js
@@ -2,6 +2,30 @@ const generateButton = document.getElementById('formula-ticket');
 generateButton.addEventListener('click',
     function() {
         const userName = document.getElementById('username').value;
-        const userDistance = document.getElementById('usertrip').value;
+        const userDistance = parseInt(document.getElementById('usertrip').value )
+        const userAge = document.getElementById('userage').value;
+        
+        // formula generale del ticket no discount
+        const price = userDistance * 0.21;
+        let discount = 0;
+        
+        // calcolo dello sconto
+        if(userAge === 'minorenne') {
+            discount = price * 20 / 100;
+        } else if (userAge === 'over') {
+            discount = price * 40 / 100;
+        }
+
+        // formula del prezzo finale matematico
+        const finalPrice = price - discount;
+
+        // converto il risultato in un numero leggibile
+        let TrainTicket = finalPrice.toFixed(2);
+
+        // STAMPO IN PAGINA
+        document.getElementById('customer-name').innerHTML = userName;
+        document.getElementById('your-ticket').innerHTML = TrainTicket;
     }
+
+
 );
