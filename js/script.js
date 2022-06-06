965ef11d55f9701d5568e3c2c5c0b77ccfb4df21
franciowel <spina_francesco@blu.it>
2022-06-06 16:29:31 +0200

    aggiunte parti Math.Rand

--- js/script.js
+++ js/script.js
@@ -22,9 +22,21 @@ generateButton.addEventListener('click',
         // converto il risultato in un numero leggibile
         let TrainTicket = finalPrice.toFixed(2);
 
+        // formula per generare codice carrozza
+        const numberWagon = Math.floor(Math.random() * 10);
+        console.log(numberWagon);
+
+        // formula per generare codice univoco
+        const CPCodeNum = Math.floor(Math.random() * 10000);
+        console.log(CPCodeNum);
+
+
         // STAMPO IN PAGINA
         document.getElementById('customer-name').innerHTML = userName;
         document.getElementById('your-ticket').innerHTML = TrainTicket;
+
+        document.getElementById('your-wagon').innerHTML = numberWagon;
+        document.getElementById('your-code-cp').innerHTML = CPCodeNum;
         document.getElementById('ticket').classList.add('active');
     }
 
