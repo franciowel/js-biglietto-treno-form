2f02260736b0ec1a563bdb08ab2a731f1b944e7b
franciowel <spina_francesco@blu.it>
2022-06-06 15:01:54 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const generateButton = document.getElementById('formula-ticket');
+generateButton.addEventListener('click',
+    function() {
+        const userName = document.getElementById('username').value;
+        const userDistance = document.getElementById('usertrip').value;
+    }
+);
